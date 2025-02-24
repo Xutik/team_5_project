1f4506e68ae6882021f6419130225ea74b4cfa28
Xutik <kseniia.prakhova@hyperisland.se>
2025-02-24 12:32:32 +0100

    Voice to text

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import VoiceToText from './voice_text';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Voice to Text App</h1>
+      <VoiceToText />
+    </div>
+  );
+}
+
+export default App;
