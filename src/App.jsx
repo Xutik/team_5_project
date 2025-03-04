5b4f196ff81db40f745cbb0601ec5853878bfcb1
Sigstus985 <siggeluren@gmail.com>
2025-03-04 12:27:30 +0100

    set up components and designed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import VoiceToText from './voice_text';
+import Navbar from './components/navbar';
+import Sidebar from './components/sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Voice to Text App</h1>
-      <VoiceToText />
+      <Navbar></Navbar>
+      <Sidebar/>
+
+      {/* <h1>Voice to Text App</h1>
+      <VoiceToText /> */}
     </div>
   );
 }
