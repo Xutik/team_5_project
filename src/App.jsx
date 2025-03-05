c86340ae7312e9bfd7100477d1aca0410eb4a035
Xutik <kseniia.prakhova@hyperisland.se>
2025-03-05 13:41:51 +0100

    Dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
 import VoiceToText from './voice_text';
+import { patientsData, staffData } from '';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Voice to Text App</h1>
       <VoiceToText />
-    </div>
+
+      <h1>Hospital Data</h1>
+      <h2>Patients</h2>
+      <ul>
+        {patientsData.map(patient => (
+         <li key={patient.Id}>{patient["Name: First"]} {patient["Name: Last"]}</li>
+       ))}
+      </ul>
+      <h2>Staff</h2>
+      <ul>
+        {staffData.map(staff => (
+          <li key={staff.Id}>{staff["Name: First"]} {staff["Name: Last"]} - {staff["Contacts: Status"]}</li>
+        ))}
+      </ul>
+   </div>
   );
 }
 
