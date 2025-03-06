614295e56ced32f4727c4a0b4504e6bdc2b9989b
Sigstus985 <siggeluren@gmail.com>
2025-03-06 13:10:56 +0100

    fixed mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar';
-import Sidebar from './components/sidebar';
-import Main from './components/main';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
+import Main from "./components/main";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Sidebar/>
-      <Main></Main>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar></Navbar>
+			<Sidebar />
+			<Main></Main>
+		</div>
+	);
 }
 
 export default App;
